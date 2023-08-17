36e27a5a3aee34464a1c5926fda00ded60ec9d7a
deniandriancode <deniandriancode@gmail.com>
2023-08-17 23:50:49 +0700

    finished the functionality

---
+++ src/app/waiting/waiting.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import validator from 'validator';
+
+@Component({
+    selector: 'app-waiting',
+    templateUrl: './waiting.component.html',
+    styleUrls: ['./waiting.component.scss']
+})
+export class WaitingComponent {
+    email = new FormControl('');
+    emailValid = false;
+    emailNonValid = false;
+
+    handleSubmitEmail() {
+	if (validator.isEmail(this.email.value)) {
+	    this.emailValid = true;
+	    this.emailNonValid = false;
+	    this.email.setValue('');
+	} else {
+	    this.emailNonValid = true;
+	    this.emailValid = false;
+	}
+    }
+}
